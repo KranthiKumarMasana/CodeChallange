e618942dee1d2a59e494329b8748786a89a147f4
KranthiKumarMasana <pearl.kranthi@gmail.com>
2020-11-12 01:44:04 -0500

    Add files via upload

---
+++ CodingChallenge.Utilities/RemoveDuplicates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingChallenge.DataAccess.Models;
+namespace CodingChallenge.Utilities
+{
+    public static class RemoveDuplicates //: IEqualityComparer<Movie>
+    {
+        //public bool Equals(Movie x, Movie y)
+        //{
+        //    return x.ID == y.ID &&
+        //        x.Title == y.Title &&
+        //        x.Year == y.Year &&
+        //        x.Rating == y.Rating;
+        //}
+
+        //public int GetHashCode(Movie obj)
+        //{
+        //    return obj.ID.GetHashCode() ^
+        //        obj.Title.GetHashCode() ^
+        //        obj.Year.GetHashCode() ^
+        //        obj.Rating.GetHashCode();
+        //}
+
+        public static IEnumerable<Movie> RemoveDuplicatesMovies(List<Movie> obj)
+        {
+            var distinct = obj.GroupBy(l => new { l.Title, l.Rating, l.Year, l.Franchise }).Select(d => new Movie
+            {
+                ID = d.First().ID,
+                Title = d.Key.Title,
+                Rating = d.Key.Rating,
+                Year = d.Key.Year,
+                Franchise = d.Key.Franchise
+            });
+
+            return distinct;
+        }
+
+    }
+}
---
+++ CodingChallenge.Utilities/StringComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CodingChallenge.DataAccess;
+using CodingChallenge.DataAccess.Models;
+
+namespace CodingChallenge.Utilities
+{
+    public static class StringComparer
+    {
+
+        public static List<T> CreateSortList<T>(IEnumerable<T> dataSource,
+                 string fieldName, SortDirection sortDirection)
+        {
+            List<T> returnList = new List<T>();
+            returnList.AddRange(dataSource);
+            PropertyInfo propInfo = typeof(T).GetProperty(fieldName);
+            Comparison<T> compare = delegate (T a, T b)
+            {
+                bool asc = sortDirection == SortDirection.Ascending;
+                object valueA = asc ? propInfo.GetValue(a, null).ToString().Replace("a", "").Replace("the", "").Replace("an", "") : propInfo.GetValue(b, null).ToString().Replace("a", "").Replace("the", "").Replace("an", "");
+                object valueB = asc ? propInfo.GetValue(b, null).ToString().Replace("a", "").Replace("the", "").Replace("an", "") : propInfo.GetValue(a, null).ToString().Replace("a", "").Replace("the", "").Replace("an", "");
+
+                return valueA is IComparable ? ((IComparable)valueA).CompareTo(valueB) : 0;
+            };
+
+
+
+            returnList.Sort(compare);
+            return returnList;
+        }
+    }
+
+    public sealed class SCompare : IComparer<Movie>
+    {
+        public int Compare(Movie x, Movie y)
+        {
+            return string.Compare(x.Title, y.Title, StringComparison.Ordinal);
+        }
+    }
+}
