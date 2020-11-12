b5f8b43de72a6d5ac33141aeea2878ec82a41fea
KranthiKumarMasana <pearl.kranthi@gmail.com>
2020-11-12 01:38:09 -0500

    Add files via upload

---
+++ CodingChallenge.UI/Controller/DefaultController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using CodingChallenge.DataAccess;
+using CodingChallenge.DataAccess.Interfaces;
+using CodingChallenge.UI.Models;
+
+namespace CodingChallenge.UI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ILibraryService LibraryService { get; private set; }
+
+        public DefaultController() { }
+
+        public DefaultController(ILibraryService libraryService)
+        {
+            LibraryService = libraryService;
+        }
+
+        public ActionResult Index([ModelBinder(typeof(GridBinder))]GridOptions options, string searchString, string SearchFranchise)
+        {
+            options.TotalItems = LibraryService.SearchMoviesCount("");
+            if (options.SortColumn == null)
+                options.SortColumn = "ID";
+            var model = new MovieListViewModel();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                model.GridOptions = options;
+                model.Movies = LibraryService.SearchMovies(searchString, (options.Page - 1) * options.ItemsPerPage, options.ItemsPerPage, options.SortColumn, options.SortDirection).ToList();
+            }
+            else if (!String.IsNullOrEmpty(SearchFranchise))
+            {
+
+                model.GridOptions = options;
+                model.Movies = LibraryService.SearchMoviesByFranchise(SearchFranchise, (options.Page - 1) * options.ItemsPerPage, options.ItemsPerPage, options.SortColumn, options.SortDirection).ToList();
+            }
+            else
+            {
+                model.GridOptions = options;
+                model.Movies = LibraryService.SortMovies((options.Page - 1) * options.ItemsPerPage, options.ItemsPerPage, options.SortColumn, options.SortDirection).ToList();
+
+            }
+
+            return View(model);
+        }
+
+
+    }
+}
---
+++ CodingChallenge.UI/Controller/MoviesController.cs
@@ -0,0 +1,82 @@
+﻿using CodingChallenge.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodingChallenge.UI.Controllers
+{
+    
+    public class MoviesController : Controller
+    {
+
+        // GET: Movies
+        public ILibraryService _service { get; private set; }
+
+        public MoviesController() { }
+
+        public MoviesController(ILibraryService libraryService)
+        {
+            _service = libraryService;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetMoviesbyTitle(string title)
+        {
+            var movies = _service.SearchMovieByTitle(title);
+            if (movies != null)
+            {
+
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchMovieByFranchise(string franchise)
+        {
+            var movies = _service.SearchMovieByFranchise(franchise);
+            if (movies != null)
+            {
+
+            }
+            return View();
+        }
+
+        public ActionResult GetMoviesByBelowRating(double rating)
+        {
+            var movies = _service.GetMoviesByBelowRating(rating);
+            if (movies != null)
+            {
+
+            }
+            return View();
+        }
+
+        public ActionResult GetMoviesByAboveRating(double rating)
+        {
+            var movies = _service.GetMoviesByAboveRating(rating);
+            if (movies != null)
+            {
+
+            }
+            return View();
+        }
+
+        public ActionResult GetMoviesByDateRange(DateTime startdate, DateTime enddate)
+        {
+            var movies = _service.GetMoviesByDateRange(startdate, enddate);
+            if (movies != null)
+            {
+
+            }
+            return View();
+        }
+
+
+    }
+}
